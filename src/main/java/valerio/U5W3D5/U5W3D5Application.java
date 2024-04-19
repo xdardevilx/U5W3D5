782c167a3f1e13d9dc8401cba361d8bb98cdcf8f
xdardevilx <valeriogalletti21@gmail.com>
2024-04-19 10:42:17 +0200

    first commit

---
+++ src/main/java/valerio/U5W3D5/U5W3D5Application.java
@@ -0,0 +1,13 @@
+package valerio.U5W3D5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5W3D5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U5W3D5Application.class, args);
+	}
+
+}
---
+++ src/test/java/valerio/U5W3D5/U5W3D5ApplicationTests.java
@@ -0,0 +1,13 @@
+package valerio.U5W3D5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5W3D5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
